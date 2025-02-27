0c276f5f93e2ef22e6887e46660d504d82b8d2c6
Imthiyas Rasheeth <mohdimthiyas.a@gmail.com>
2025-02-27 10:40:03 +0000

    develop

--- LoopFxDemo/app.py
+++ LoopFxDemo/app.py
@@ -10,32 +10,38 @@ import plotly.graph_objects as go
 import yfinance as yf
 from config import Config
 
-# Print current working directory and sys.path for debugging
-print("Current Working Directory:", os.getcwd())
-print("sys.path:", sys.path)
+# Setting up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Log current working directory and sys.path for debugging
+logger.info("Current Working Directory: %s", os.getcwd())
+logger.info("sys.path: %s", sys.path)
 
 # Add the parent directory of LoopFxDemo to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 app = Flask(__name__)
-# Load configuration into your Flask app
+# Load configuration into the Flask app
 app.config.from_object(Config)
 
 # Setting up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Home page route that calls the same function with a default pair and period
+@app.route('/', defaults={'pair': 'EURUSD', 'period': 7})
 @app.route('/home', defaults={'pair': 'EURUSD', 'period': 7})
-@app.route('/home/<pair>/<int:period>')
+@app.route('/home/<pair>/<int:period>') # Dynamic route that accepts a currency pair and period
 def home(pair, period):
-    """Render the home page with the candlestick chart."""
+    # Render the home page with the candlestick chart.
     logger.info("Initializing home route")
     data = []  # Initialize the data variable as an empty list
     graph_json = None
     message = None
     try:
-          # Fetch data from the backend API
+        # Fetch data from the backend API
         data = fetch_data_from_api(pair, period)
         graph_json= getGraphJson(data, pair, period)
         logger.info(f"Generated graph JSON: {graph_json}")
@@ -47,8 +53,9 @@ def home(pair, period):
     # Render the data in the template
     return render_template('home.html', graph_json=graph_json, data=data, currency=pair, period=period, message=message)
 
+
 def fetch_data_from_api(pair, period):
-    """Fetch data from the backend API."""
+    # Fetch data from the backend API.
     api_url = f"{app.config['BACKEND_API_URL']}/currency_prices_by_pair_and_period/{pair}/{period}"
     print(api_url)
     response = requests.get(api_url)
@@ -58,7 +65,6 @@ def fetch_data_from_api(pair, period):
     return data
 
 def getGraphJson(data, pair, period):
-    
     # Create a DataFrame
     if isinstance(data, dict):
             fxdata_df = pd.DataFrame([data])
@@ -68,17 +74,17 @@ def getGraphJson(data, pair, period):
     if 'time' in fxdata_df.columns:
             
         fxdata_df['time'] = pd.to_datetime(fxdata_df['time'], utc=True).dt.tz_convert('UTC').dt.tz_localize(None)
-                
-                # Calculate the 20-period SMA and EMA
+        
+        # Calculate the 20-period SMA and EMA
         fxdata_df['SMA20'] = fxdata_df['close'].rolling(window=20).mean()
         fxdata_df['EMA20'] = fxdata_df['close'].ewm(span=20, adjust=False).mean()
                 
-                # Calculate Bollinger Bands
+        # Calculate Bollinger Bands
         fxdata_df['std'] = fxdata_df['close'].rolling(window=20).std()
         fxdata_df['UpperBand'] = fxdata_df['SMA20'] + (fxdata_df['std'] * 2)
         fxdata_df['LowerBand'] = fxdata_df['SMA20'] - (fxdata_df['std'] * 2)
         
-                
+        # Create a candlestick chart using Plotly
         chart_figure = go.Figure()
 
         chart_figure.add_trace(go.Candlestick(
@@ -92,7 +98,7 @@ def getGraphJson(data, pair, period):
                     decreasing_line_color='red'
                 ))
 
-                # SMA20
+        # SMA20
         chart_figure.add_trace(go.Scatter(
                     x=fxdata_df['time'],
                     y=fxdata_df['SMA20'],
@@ -101,7 +107,7 @@ def getGraphJson(data, pair, period):
                     name='SMA20'
                 ))
 
-                # EMA20
+        # EMA20
         chart_figure.add_trace(go.Scatter(
                     x=fxdata_df['time'],
                     y=fxdata_df['EMA20'],
@@ -110,7 +116,7 @@ def getGraphJson(data, pair, period):
                     name='EMA20'
                 ))
 
-                # Upper Band (on second axis)
+        # Upper Band
         chart_figure.add_trace(go.Scatter(
                     x=fxdata_df['time'],
                     y=fxdata_df['UpperBand'],
@@ -121,7 +127,7 @@ def getGraphJson(data, pair, period):
                     opacity=0.7
                 ))
 
-                # Lower Band (on second axis) with fill
+        # Lower Band with fill
         chart_figure.add_trace(go.Scatter(
                     x=fxdata_df['time'],
                     y=fxdata_df['LowerBand'],
@@ -134,7 +140,7 @@ def getGraphJson(data, pair, period):
                     opacity=0.7
                 ))
 
-                # Layout updates: template, margin, axes, interactive legend
+        # Layout updates: template, margin, axes, interactive legend
         chart_figure.update_layout(
                     title=f'Candlestick Chart for {pair} - {period} days',
                     xaxis_title='Time',
@@ -156,7 +162,7 @@ def getGraphJson(data, pair, period):
                     )
                 )
 
-                # Convert the chart to JSON for rendering
+        # Convert the chart to JSON for rendering
         graph_json = chart_figure.to_json()
         print(graph_json)
         logger.info("Chart JSON generated")
@@ -166,6 +172,19 @@ def getGraphJson(data, pair, period):
             logger.warning(message)
             return None
 
+
+# API endpoint to get chart data for a currency pair and period, which only returns JSON data to be used in the frontend (Using Ajax).
+@app.route('/api/chart-data/<pair>/<int:period>', methods=['GET'])
+def chart_data(pair, period):
+    # Fetch data from the backend API
+    data = fetch_data_from_api(pair, period)
+    graph_json= getGraphJson(data, pair, period)
+    logger.info(f"Generated graph JSON2: {graph_json}")
+    return jsonify(graph_json)
+
+
+
+# Function to fetch live currency rates from Yahoo Finance
 def fetch_live_rates():
     tickers = {
         "EURUSD": "EURUSD=X",
@@ -187,19 +206,13 @@ def fetch_live_rates():
     return live_data
 
 
+# API endpoint to get live currency rates
 @app.route('/api/live-rates')
 def live_rates():
     rates = fetch_live_rates()
     return jsonify(rates)
 
 
-@app.route('/api/chart-data/<pair>/<int:period>', methods=['GET'])
-def chart_data(pair, period):
-    # Fetch data from the backend API
-    data = fetch_data_from_api(pair, period)
-    graph_json= getGraphJson(data, pair, period)
-    logger.info(f"Generated graph JSON2: {graph_json}")
-    return jsonify(graph_json)
 
 if __name__ == '__main__':
     logger.info("App configuration:")
